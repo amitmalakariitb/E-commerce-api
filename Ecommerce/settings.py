87cdcbc9ccd0a74ff31d6c7ad0bb9ee7a7ea2a49
Haris Narrendran <haris@iitb.com>
2024-02-17 02:22:42 +0530

    added user api

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'user',
+    'django_rest_passwordreset',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    # Other settings...
+}
+
---
+++ user/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    mobile_number = models.CharField(max_length=10)
+    # Add custom fields here, if needed
+    def __str__(self):
+        return self.username
