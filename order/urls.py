d4912c21ecd74c52003c4470b4d76d88192402b9
Haris Narrendran <haris@iitb.com>
2024-03-12 03:31:16 +0530

    user address and prime user

--- order/urls.py
+++ order/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     path('add_to_cart/' , add_to_cart,name = 'add_to_cart'),
     path('view_cart/' , view_cart,name = 'view_cart'),
     path('edit_cart/<str:pk>' , edit_cart,name = 'edit_cart'),
-    path('Direct_Purchase/' , direct_purchase,name = 'create_order'),
-    path('Checkout/' , checkout,name = 'create_order_from_cart'),
+    path('direct_purchase/' , direct_purchase,name = 'create_order'),
+    path('checkout/' , checkout,name = 'create_order_from_cart'),
 ]
